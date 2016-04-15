1c73a236fe6179e9e2832820b8abeccc1b474438
Nikhil Thota <nt92@ufl.edu>
2016-04-15 13:35:44 -0400

    added initial project for data struc initial problems

---
+++ Assignment 5/Assignment 5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment 5
+//
+//  Created by Nikhil Thota on 4/15/16.
+//  Copyright © 2016 Nikhil Thota. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
