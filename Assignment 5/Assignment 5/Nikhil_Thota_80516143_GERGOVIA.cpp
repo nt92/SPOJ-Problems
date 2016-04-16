14e8a45f36290fce32ee554ce19f9617e60c918a
Nikhil Thota <nt92@ufl.edu>
2016-04-16 17:09:30 -0400

    added linkedlist barebones for problem

--- Assignment 5/Assignment 5/Nikhil_Thota_80516143_GERGOVIA.cpp
+++ Assignment 5/Assignment 5/Nikhil_Thota_80516143_GERGOVIA.cpp
@@ -6,4 +6,76 @@
 //  Copyright © 2016 Nikhil Thota. All rights reserved.
 //
 
-#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next = NULL;
+    
+    Node(int d)
+    {
+        data = d;
+    }
+    
+    Node(int d, Node *n)
+    {
+        data = d;
+        next = n;
+    }
+};
+
+class LinkedList
+{
+private:
+    Node *tail = NULL;
+    
+public:
+    Node *head = NULL;
+    
+    void addNode(int val)
+    {
+        Node *p;
+        
+        if(!head)
+        {
+            head = new Node(val);
+            tail = head;
+        }
+        else
+        {
+            p = tail;
+            p->next = new Node(val);
+            tail = p->next;
+        }
+    }
+    
+    void printList()
+    {
+        Node *p = head;
+        while(p)
+        {
+            cout << p->data << "\n";
+            p = p->next;
+        }
+    }
+};
+
+int main()
+{
+    int n; cin >> n;
+    
+    LinkedList output;
+    
+    while(n != 0)
+    {
+        LinkedList list;
+        
+        for(int i = 0; i < n; i++)
+        {
+            int in; cin >> in;
+            list.addNode(in);
+        }
+    }
+}
