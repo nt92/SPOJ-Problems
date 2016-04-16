b515a080720ef958a48e4d964f38a0f666963123
Nikhil Thota <nt92@ufl.edu>
2016-04-16 14:07:29 -0400

    added merge sort

--- Assignment 5/Assignment 5/Nikhil_Thota_80516143_INVCNT.cpp
+++ Assignment 5/Assignment 5/Nikhil_Thota_80516143_INVCNT.cpp
@@ -6,4 +6,78 @@
 //  Copyright © 2016 Nikhil Thota. All rights reserved.
 //
 
-#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+void Merge(int *L, int *R, int *A, int lenL, int lenR)
+{
+    int i = 0; int j = 0; int k = 0;
+    while(i < lenL && j < lenR)
+    {
+        if(L[i] <= R[j])
+        {
+            A[k] = L[i];
+            i++;
+        }
+        else
+        {
+            A[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while(i < lenL)
+    {
+        A[k] = L[i];
+        i++;
+        k++;
+    }
+    while(j < lenR)
+    {
+        A[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+
+int MergeSort(int *A, int lenA)
+{
+    int inv = 0;
+    if(lenA < 2)
+        return inv;
+    
+    int mid = lenA/2;
+    int left[mid];
+    int right[lenA - mid];
+    
+    for(int i = 0; i < mid; i++)
+    {
+        int temp = A[i];
+        left[i] = temp;
+    }
+    
+    for(int i = 0; i < lenA - mid; i++)
+    {
+        int temp = A[mid + i];
+        right[i] = temp;
+    }
+    
+    MergeSort(left, mid);
+    MergeSort(right, lenA - mid);
+    Merge(left, right, A, mid, lenA - mid);
+}
+
+int main()
+{
+    int array[4];
+    array[0] = 5; array[1] = 4; array[2] = 3; array[3] = 2;
+    
+    MergeSort(array, 4);
+    
+    for(int i = 0; i < 4; i++)
+        cout << array[i] << " ";
+    
+    return 0;
+}
+
--- Assignment 5/Assignment 5/Nikhil_Thota_80516143_PTO7Y.cpp
+++ Assignment 5/Assignment 5/Nikhil_Thota_80516143_PTO7Y.cpp
@@ -1,91 +1,91 @@
+////
+////  Nikhil_Thota_80516143_PTO7Y.cpp
+////  Assignment 5
+////
+////  Created by Nikhil Thota on 4/15/16.
+////  Copyright © 2016 Nikhil Thota. All rights reserved.
+////
 //
-//  Nikhil_Thota_80516143_PTO7Y.cpp
-//  Assignment 5
+//#include <iostream>
+//#include <list>
+//#include <string>
+//using namespace std;
 //
-//  Created by Nikhil Thota on 4/15/16.
-//  Copyright © 2016 Nikhil Thota. All rights reserved.
+//class Graph
+//{
+//private:
+//    //Initialize N, number of nodes and the adjacency list
+//    int N;
+//    list<int> *adjacencyList;
+//    
+//    void DFSNode(int n, bool visitedList[])
+//    {
+//        //Mark node n as visited
+//        visitedList[n] = true;
+//        
+//        //Iterate through the adjacency list of n and recursively visit each node adjacent to n
+//        list<int>::iterator i;
+//        for(i = adjacencyList[n].begin(); i != adjacencyList[n].end(); i++)
+//        {
+//            if(!visitedList[*i])
+//                DFSNode(*i, visitedList);
+//            else if(visitedList[*i])
+//                treeFlag = "NO";
+//        }
+//    }
+//    
+//public:
+//    //Flag for whether or not the Graph is a tree
+//    string treeFlag = "YES";
+//    
+//    //Initialize a new graph with N nodes and create an adjacency list for each node
+//    Graph(int N)
+//    {
+//        //N + 1 so that there is no index 0 error
+//        this->N = N + 1;
+//        adjacencyList = new list<int>[N + 1];
+//    }
+//    
+//    //Connect a and b
+//    void addEdge(int a, int b)
+//    {
+//        adjacencyList[a].push_back(b);
+//        //adjacencyList[b].push_back(a);
+//    }
+//    
+//    //Does a DFS of the whole graph
+//    void DFS()
+//    {
+//        bool *visitedList = new bool[N];
+//        for(int i = 0; i < N; i++)
+//            visitedList[i] = false;
+//        
+//        for(int i = 0; i < N; i++)
+//        {
+//            if(visitedList[i] == false)
+//                DFSNode(i, visitedList);
+//        }
+//    }
+//};
 //
-
-#include <iostream>
-#include <list>
-#include <string>
-using namespace std;
-
-class Graph
-{
-private:
-    //Initialize N, number of nodes and the adjacency list
-    int N;
-    list<int> *adjacencyList;
-    
-    void DFSNode(int n, bool visitedList[])
-    {
-        //Mark node n as visited
-        visitedList[n] = true;
-        
-        //Iterate through the adjacency list of n and recursively visit each node adjacent to n
-        list<int>::iterator i;
-        for(i = adjacencyList[n].begin(); i != adjacencyList[n].end(); i++)
-        {
-            if(!visitedList[*i])
-                DFSNode(*i, visitedList);
-            else if(visitedList[*i])
-                treeFlag = "NO";
-        }
-    }
-    
-public:
-    //Flag for whether or not the Graph is a tree
-    string treeFlag = "YES";
-    
-    //Initialize a new graph with N nodes and create an adjacency list for each node
-    Graph(int N)
-    {
-        //N + 1 so that there is no index 0 error
-        this->N = N + 1;
-        adjacencyList = new list<int>[N + 1];
-    }
-    
-    //Connect a and b
-    void addEdge(int a, int b)
-    {
-        adjacencyList[a].push_back(b);
-        //adjacencyList[b].push_back(a);
-    }
-    
-    //Does a DFS of the whole graph
-    void DFS()
-    {
-        bool *visitedList = new bool[N];
-        for(int i = 0; i < N; i++)
-            visitedList[i] = false;
-        
-        for(int i = 0; i < N; i++)
-        {
-            if(visitedList[i] == false)
-                DFSNode(i, visitedList);
-        }
-    }
-};
-
-int main(int argc, const char * argv[])
-{
-    int N, M;
-    cin >> N >> M;
-    
-    Graph DFSGraph(N);
-    
-    for(int i = 0; i < M; i++)
-    {
-        int a, b;
-        cin >> a >> b;
-        
-        DFSGraph.addEdge(a, b);
-    }
-    
-    DFSGraph.DFS();
-    
-    cout << DFSGraph.treeFlag;
-    
-    return 0;
-}
+//int main(int argc, const char * argv[])
+//{
+//    int N, M;
+//    cin >> N >> M;
+//    
+//    Graph DFSGraph(N);
+//    
+//    for(int i = 0; i < M; i++)
+//    {
+//        int a, b;
+//        cin >> a >> b;
+//        
+//        DFSGraph.addEdge(a, b);
+//    }
+//    
+//    DFSGraph.DFS();
+//    
+//    cout << DFSGraph.treeFlag;
+//    
+//    return 0;
+//}
